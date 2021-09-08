8443fc6bc3e8a0c43e867474fbac1ef6f18fdbe9
Dan <dgrowns@gmail.com>
2021-09-08 11:34:36 +0100

    Query handler interfaces which don't require an input parameter

--- src/TinyCqrs/Interfaces/IQueryHandler.cs
+++ src/TinyCqrs/Interfaces/IQueryHandler.cs
@@ -2,12 +2,34 @@ using System.Threading.Tasks;
 
 namespace TinyCqrs.Interfaces
 {
+    /// <summary>
+    /// A query handler that takes no parameters and returns a result
+    /// </summary>
+    public interface IQueryHandler<TResult>
+    {
+        TResult Get();
+    }
+    
+    /// <summary>
+    /// A query handler that takes a parameter and returns a result. Type checks the query against the result.
+    /// </summary>
     public interface IQueryHandler<TQuery, TResult>
         where TQuery : IQuery<TResult>
     {
         TResult Get(TQuery query);
     }
     
+    /// <summary>
+    /// An asynchronous query handler that takes no parameters and returns a result
+    /// </summary>
+    public interface IQueryHandlerAsync<TResult>
+    {
+        Task<TResult> Get();
+    }
+
+    /// <summary>
+    /// An asynchronous query handler that takes a parameter and returns a result. Type checks the query against the result.
+    /// </summary>
     public interface IQueryHandlerAsync<TQuery, TResult>
         where TQuery : IQuery<TResult>
     {
