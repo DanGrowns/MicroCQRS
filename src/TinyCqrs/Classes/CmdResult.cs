1402e33f69c2182dd17d6a2033c871b8079f7ed7
Dan Growns <d.growns@whitbywood.com>
2024-03-01 16:15:35 +0000

    Minor fixes
    4.01, 1.02 - small changes to match what I'm looking for

--- src/TinyCqrs/Classes/CmdResult.cs
+++ src/TinyCqrs/Classes/CmdResult.cs
@@ -12,9 +12,31 @@ namespace TinyCqrs.Classes
     {
         [ExcludeFromCodeCoverage]
         public CmdResult() => Issues = new List<CmdIssue>();
+
         public CmdResult(string type) : this() => Type = type;
+        public CmdResult(string type, CmdIssue issue) : this()
+        {
+            Type = type;
+            Issues.Add(issue);
+        }
+        public CmdResult(string type, string issueMessage) : this()
+        {
+            Type = type;
+            Issues.Add(new CmdIssue(issueMessage));
+        }
+
         public CmdResult(Enum enumValue) : this() => Type = enumValue.ToString();
-        
+        public CmdResult(Enum enumValue, CmdIssue issue) : this() 
+        {
+            Type = enumValue.ToString();
+            Issues.Add(issue);
+        }
+        public CmdResult(Enum enumValue, string issueMessage) : this()
+        {
+            Type = enumValue.ToString();
+            Issues.Add(new CmdIssue(issueMessage));
+        }
+
         [JsonConstructor]
         public CmdResult(string type, List<CmdIssue> issues) : this()
         {
