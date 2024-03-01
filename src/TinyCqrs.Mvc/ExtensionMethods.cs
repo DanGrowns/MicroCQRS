308f4a9d8918e821fc640f485da83976eaf7548c
Dan Growns <d.growns@whitbywood.com>
2024-03-01 15:28:15 +0000

    TinyCqrs.Mvc 1.0.0

---
+++ src/TinyCqrs.Mvc/ExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using TinyCqrs.Interfaces;
+
+namespace TinyCqrs.Mvc;
+
+public static class ExtensionMethods
+{
+    private static HttpStatusCode StatusCodeFromCmdResult<TOutput>(ICmdResult<TOutput> cmdResult)
+    {
+        var parsed = Enum.TryParse<HttpStatusCode>(cmdResult.Type, out var statusCode);
+        if (parsed)
+            return statusCode;
+
+        return cmdResult.Success ? HttpStatusCode.OK : HttpStatusCode.BadRequest;
+    }
+
+    public static IActionResult ToActionResult<TOutput>(this ICmdResult<TOutput> cmdResult)
+    {
+        var statusCode = StatusCodeFromCmdResult(cmdResult);
+        var statusCodeInt = Convert.ToInt32(statusCode);
+
+        return cmdResult.Output == null 
+            ? new StatusCodeResult(statusCodeInt) 
+            : new ObjectResult(cmdResult.Output) { StatusCode = statusCodeInt };
+    }
+}
